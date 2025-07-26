88377feb18e3dab4462095fdfcd4073c74facb4f
adhika gunadarma <adhika.gunadarma@traveloka.com>
2025-07-26 17:31:19 +0700

    for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'wedding-page';
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '',
+
   // (optional) Add other configs here, like trailingSlash if needed:
   // trailingSlash: true,
 };
