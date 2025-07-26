b09e1d3f03ae5ff2c3978deb4f4e82a8e707121c
adhika gunadarma <adhika.gunadarma@traveloka.com>
2025-07-26 17:52:49 +0700

    update for s3 and gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const repoName = 'wedding-page';
+const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '',
+  basePath: process.env.NODE_ENV === 'production' && isGithubPages ? `/${repoName}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' && isGithubPages ? `/${repoName}/` : '',
 
   // (optional) Add other configs here, like trailingSlash if needed:
   // trailingSlash: true,
