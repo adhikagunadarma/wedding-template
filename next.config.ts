cfa6caf1d7570900dbdc1f683117251f01adf509
adhika gunadarma <adhika.gunadarma@traveloka.com>
2025-07-27 17:15:24 +0700

    TRY

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-const repoName = 'wedding-template';
-const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+// const repoName = 'wedding-template';
+// const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,8 +8,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: '/wedding-template',
+  assetPrefix: '/wedding-template/',
 };
 
 export default nextConfig;
